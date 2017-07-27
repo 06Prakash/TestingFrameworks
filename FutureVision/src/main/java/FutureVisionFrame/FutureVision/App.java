732d3df7a3155324bdb34c2706eeec47e5c92b82
Chandra Prakash V <chandraprakashveluchamy@gmail.com>
2017-07-28 00:11:34 +0530

    Frame work structure
    Just a start not completely working stuffs.

---
+++ FutureVision/src/main/java/FutureVisionFrame/FutureVision/App.java
@@ -0,0 +1,13 @@
+package FutureVisionFrame.FutureVision;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
